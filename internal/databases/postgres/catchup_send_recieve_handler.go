169ddbf1e36faa96b811972ed1ac5e3899183e01
Andrey M. Borodin <x4mmm@night.local>
2024-03-03 22:42:56 +0500

    Move on

--- internal/databases/postgres/catchup_send_recieve_handler.go
+++ internal/databases/postgres/catchup_send_recieve_handler.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/internal"
+	"github.com/wal-g/wal-g/internal/ioextensions"
 	"github.com/wal-g/wal-g/utility"
+	"io"
 	"io/fs"
 	"net"
 	"os"
 	"path"
 	"path/filepath"
+	"time"
 )
 
 func HandleCatchupSend(pgDataDirectory string, destination string) {
@@ -20,13 +23,13 @@ func HandleCatchupSend(pgDataDirectory string, destination string) {
 	if info.systemIdentifier == nil {
 		tracelog.ErrorLogger.Fatal("Our system lacks System Identifier, cannot proceed")
 	}
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	dial, err := net.Dial("tcp", destination)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	decoder := gob.NewDecoder(dial)
 	var control PgControlData
 	err = decoder.Decode(&control)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	tracelog.InfoLogger.Printf("Destination control file %v", control)
 	tracelog.InfoLogger.Printf("Our system id %v", *info.systemIdentifier)
 	if *info.systemIdentifier != control.SystemIdentifier {
@@ -37,12 +40,12 @@ func HandleCatchupSend(pgDataDirectory string, destination string) {
 	}
 	var fileList internal.BackupFileList
 	err = decoder.Decode(&fileList)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	tracelog.InfoLogger.Printf("Received file list of %v failes", len(fileList))
 	_, lsnStr, _, err := runner.StartBackup("")
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	lsn, err := ParseLSN(lsnStr)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	if lsn <= control.Checkpoint {
 		tracelog.ErrorLogger.Fatal("Catchup destination is already ahead (our LSN %v, destination LSN %v).", lsn, control.Checkpoint)
 	}
@@ -50,21 +53,21 @@ func HandleCatchupSend(pgDataDirectory string, destination string) {
 	encoder := gob.NewEncoder(dial)
 
 	label, offsetMap, _, err := runner.StopBackup()
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 
 	sendFileCommand(dial, encoder, pgDataDirectory, fileList, control.Checkpoint)
 
-	err = encoder.Encode(CatchupCommandDto{Contents: label, FileName: BackupLabelFilename, IsStr: true})
-	tracelog.ErrorLogger.FatalOnError(err)
-	err = encoder.Encode(CatchupCommandDto{Contents: offsetMap, FileName: TablespaceMapFilename, IsStr: true})
-	tracelog.ErrorLogger.FatalOnError(err)
+	err = encoder.Encode(CatchupCommandDto{BinaryContents: []byte(label), FileName: BackupLabelFilename, IsBinContents: true})
+	tracelog.ErrorLogger.PanicOnError(err)
+	err = encoder.Encode(CatchupCommandDto{BinaryContents: []byte(offsetMap), FileName: TablespaceMapFilename, IsBinContents: true})
+	tracelog.ErrorLogger.PanicOnError(err)
 	ourPgControl, err := os.ReadFile(path.Join(pgDataDirectory, PgControlPath))
-	tracelog.ErrorLogger.FatalOnError(err)
-	err = encoder.Encode(CatchupCommandDto{BinaryContents: ourPgControl, FileName: PgControlPath})
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
+	err = encoder.Encode(CatchupCommandDto{BinaryContents: ourPgControl, FileName: utility.SanitizePath(PgControlPath), IsBinContents: true})
+	tracelog.ErrorLogger.PanicOnError(err)
 
 	err = encoder.Encode(CatchupCommandDto{IsDone: true})
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	tracelog.InfoLogger.Printf("Send done")
 }
 
@@ -86,6 +89,9 @@ func sendFileCommand(dial net.Conn, encoder *gob.Encoder, directory string, list
 		if isDir && excluded {
 			return filepath.SkipDir
 		}
+		if isDir {
+			return nil
+		}
 		if excluded {
 			return nil
 		}
@@ -98,22 +104,47 @@ func sendFileCommand(dial net.Conn, encoder *gob.Encoder, directory string, list
 			}
 		}
 
+		fd, err := os.Open(path)
+		tracelog.ErrorLogger.PanicOnError(err)
+		size := info.Size()
+		err = encoder.Encode(CatchupCommandDto{FileName: fullFileName, IsFull: true, FileSize: uint64(size)})
+		tracelog.ErrorLogger.PanicOnError(err)
+		reader := &io.LimitedReader{
+			R: io.MultiReader(fd, &ioextensions.ZeroReader{}),
+			N: size,
+		}
+
+		n, err := utility.FastCopy(dial, reader)
+		tracelog.InfoLogger.Printf("Sent %v, %v bytes", fullFileName, n)
+		if n != size {
+			tracelog.ErrorLogger.Fatalf("Short write %v instead of %v", n, size)
+		}
+		tracelog.ErrorLogger.PanicOnError(err)
+		err = fd.Close()
+		tracelog.ErrorLogger.PanicOnError(err)
+
+		time.Sleep(time.Second)
+
 		return nil
 	})
+	tracelog.DebugLogger.Printf("Filepath walk done")
 }
 
 func HandleCatchupReceive(pgDataDirectory string, port int) {
 	pgDataDirectory = utility.ResolveSymlink(pgDataDirectory)
 	tracelog.InfoLogger.Printf("Receiving %v on port %v\n", pgDataDirectory, port)
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	conn, err := listen.Accept()
 	sendControlAndFileList(pgDataDirectory, err, conn)
 	decoder := gob.NewDecoder(conn)
 	for {
 		var cmd CatchupCommandDto
 		err := decoder.Decode(&cmd)
-		tracelog.ErrorLogger.FatalOnError(err)
+		if io.EOF == err {
+			break
+		}
+		tracelog.ErrorLogger.PanicOnError(err)
 		if cmd.IsDone {
 			break
 		}
@@ -123,42 +154,51 @@ func HandleCatchupReceive(pgDataDirectory string, port int) {
 }
 
 func doRcvCommand(cmd CatchupCommandDto, directory string, conn net.Conn) {
-	if cmd.BinaryContents != nil {
+	if cmd.IsBinContents {
 		tracelog.InfoLogger.Printf("Writing file %v", cmd.FileName)
 		err := os.WriteFile(path.Join(directory, cmd.FileName), cmd.BinaryContents, 0666)
-		tracelog.ErrorLogger.FatalOnError(err)
+		tracelog.ErrorLogger.PanicOnError(err)
 		return
 	}
-	if cmd.IsStr {
-		tracelog.InfoLogger.Printf("Writing file %v", cmd.FileName)
-		err := os.WriteFile(path.Join(directory, cmd.FileName), []byte(cmd.Contents), 0666)
-		tracelog.ErrorLogger.FatalOnError(err)
-		return
+
+	if cmd.IsFull {
+		tracelog.InfoLogger.Printf("Full file %v", cmd.FileName)
+		fd, err := os.Create(path.Join(directory, cmd.FileName))
+		tracelog.ErrorLogger.PanicOnError(err)
+		size := int64(cmd.FileSize)
+		n, err := utility.FastCopy(fd, &io.LimitedReader{R: conn, N: size})
+		if n != size {
+			tracelog.InfoLogger.Printf("Received %v bytes instead of %v", n, size)
+		}
+		tracelog.InfoLogger.Printf("Received %v bytes", n)
+		tracelog.ErrorLogger.PanicOnError(err)
+		err = fd.Close()
+		tracelog.ErrorLogger.PanicOnError(err)
 	}
 }
 
 type CatchupCommandDto struct {
 	IsDone         bool
 	IsIncremental  bool
+	IsFull         bool
 	IsDelete       bool
-	IsStr          bool
+	IsBinContents  bool
 	FileSize       uint64
 	FileName       string
-	Contents       string
 	BinaryContents []byte
 }
 
 func sendControlAndFileList(pgDataDirectory string, err error, conn net.Conn) {
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	control, err := ExtractPgControl(pgDataDirectory)
 	tracelog.InfoLogger.Printf("Our system id %v, need catchup from %v", control.SystemIdentifier, control.Checkpoint)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	encoder := gob.NewEncoder(conn)
 	err = encoder.Encode(control)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 	rcvFileList := receiveFileList(pgDataDirectory)
 	err = encoder.Encode(rcvFileList)
-	tracelog.ErrorLogger.FatalOnError(err)
+	tracelog.ErrorLogger.PanicOnError(err)
 }
 
 func receiveFileList(directory string) internal.BackupFileList {
